23bd2dcfca798f8234493e1ca855200950795f5f
Sterling Jones <sterling@ev.io>
2019-07-26 08:19:04 -0400

    Updating copywrite and removing IDE files.

--- rel/config.exs
+++ rel/config.exs
@@ -2,6 +2,24 @@
 # They can then be used by adding `plugin MyPlugin` to
 # either an environment, or release definition, where
 # `MyPlugin` is the name of the plugin module.
+
+## 
+# Author:     Sterling Stanford-Jones
+# Copyright:      Copyright (C) 2019  <name of author>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#             
 Path.join(["rel", "plugins", "*.exs"])
 |> Path.wildcard()
 |> Enum.map(&Code.eval_file(&1))
